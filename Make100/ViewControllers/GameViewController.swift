e1c7171daed35776a6b013d76ed987ebff36c0be
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-09-15 11:49:20 +0900

    ▶︎SkipButtonの実装 ▶︎Resultの初期値を-10〜10に設定

--- Make100/ViewControllers/GameViewController.swift
+++ Make100/ViewControllers/GameViewController.swift
@@ -17,12 +17,16 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstNum: Int = generateRandomNumber(min: 0, max: 40, delta: 20)//初期値として-20から20の整数取得
-        calcLabel.text = String(firstNum)
+        let firstCalcNum: Int = generateRandomNumber(min: 0, max: 40, delta: 20)//初期値として-20から20の整数取得
+        let firseResultNum: Int = generateRandomNumber(min: 0, max: 20, delta: 10)//初期値として-10から10の整数取得
+        calcLabel.text = String(firstCalcNum)
+        resultLabel.text = String(firseResultNum)
         
 
     }
 
+    
+    ///===============各Buttonのアクション===============
     //『+』ボタン押した時のアクション
     @IBAction func PlusButton() {
         print("Pushed PlusButton")
@@ -56,96 +60,17 @@ class GameViewController: UIViewController {
         calcLabel.text = String(generateRandomNumber(min: 0, max: 40, delta: 20))//計算と同時に次の数字をランダム表示
     }
     
+    @IBAction func SkipButton()
+    {
+        print("Pushed SkipButton")
+        calcLabel.text = String(generateRandomNumber(min: 0, max: 40, delta: 20))//計算と同時に次の数字をランダム表示
+    }
+    
+    
+    ///===============その他メソッド===============
     //乱数を発生させて、Int型で返してくれるメソッド(deltaで数字の調整できる)
     func generateRandomNumber(min: UInt32, max: UInt32, delta: Int) -> Int {
         return Int(arc4random_uniform(UInt32(max - min + 1)) + min) - delta
     }
-    
-    // MARK: カスタムボタンの定義
-    @IBDesignable
-    class CustomButton: UIButton {
-        
-        // 角丸の半径(0で四角形)
-        @IBInspectable var cornerRadius: CGFloat = 0.0
-        
-        // 枠
-        @IBInspectable var borderColor: UIColor = UIColor.clear
-        @IBInspectable var borderWidth: CGFloat = 0.0
-        
-        // バックグラウンドカラー
-        @IBInspectable var nonHighlightedBackgroundColor :UIColor?
-        @IBInspectable var highlightedBackgroundColor :UIColor?
-        
-        override func draw(_ rect: CGRect) {
-            // 角丸
-            self.layer.cornerRadius = cornerRadius
-            self.clipsToBounds = (cornerRadius > 0)
-            
-            // 枠線
-            self.layer.borderColor = borderColor.cgColor
-            self.layer.borderWidth = borderWidth
-            
-            super.draw(rect)
-        }
-        
-        // バックグラウンドカラー
-        override var isHighlighted :Bool {
-            didSet {
-                if isHighlighted {
-                    self.backgroundColor = highlightedBackgroundColor
-                }
-                else {
-                    self.backgroundColor = nonHighlightedBackgroundColor
-                }
-            }
-        }
-    }
-    
-    // MARK: カスタムラベルの定義
-    @IBDesignable class CustomLabel: UILabel {
-        
-        // 角丸の半径(0で四角形)
-        @IBInspectable var cornerRadius: CGFloat = 0.0
-        
-        // 枠
-        @IBInspectable var borderColor: UIColor = UIColor.clear
-        @IBInspectable var borderWidth: CGFloat = 0.0
-        
-        override func draw(_ rect: CGRect) {
-            // 角丸
-            self.layer.cornerRadius = cornerRadius
-            self.clipsToBounds = (cornerRadius > 0)
-            
-            // 枠線
-            self.layer.borderColor = borderColor.cgColor
-            self.layer.borderWidth = borderWidth
-            
-            super.draw(rect)
-        }
-    }
-    
-    // MARK: カスタムテキストビューの定義
-    @IBDesignable class CustomTextView: UITextView {
-        
-        // 角丸の半径(0で四角形)
-        @IBInspectable var cornerRadius: CGFloat = 0.0
-        
-        // 枠
-        @IBInspectable var borderColor: UIColor = UIColor.clear
-        @IBInspectable var borderWidth: CGFloat = 0.0
-        
-        override func draw(_ rect: CGRect) {
-            // 角丸
-            self.layer.cornerRadius = cornerRadius
-            self.clipsToBounds = (cornerRadius > 0)
-            
-            // 枠線
-            self.layer.borderColor = borderColor.cgColor
-            self.layer.borderWidth = borderWidth
-            
-            super.draw(rect)
-        }
-    }
-
     
 }
